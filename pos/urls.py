d1cd6cd72b5e240e57bfdc8d7d1c849fd7afa64f
codevibrant23 <code.vibrantdigitech@gmail.com>
2024-09-18 23:33:57 +0530

    latest api including the qr api

--- pos/urls.py
+++ pos/urls.py
@@ -38,6 +38,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('v1/api/', include('v1.urls')),
+    path('v1/api/qr/', include('qr.urls')),
     path('v1/auth/', include('userauth.urls')),
     path('redoc/', schema_view.with_ui('redoc',cache_timeout=0), name='schema-redoc'),
     path('swagger/', schema_view.with_ui('swagger',cache_timeout=0), name='schema-swagger-ui'),
